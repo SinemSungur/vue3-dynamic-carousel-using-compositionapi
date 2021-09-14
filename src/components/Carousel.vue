29c563943946411cf34f516245eed3c56351ce8d
SinemS <ssinemsungur@gmail.com>
2021-09-14 14:59:10 +0300

    Layout UI added

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,12 +1,29 @@
 <template>
   <div class="carousel">
-    <slot> </slot>
+    <slot :currentSlide="currentSlide"> </slot>
+
+    <div class="navigation">
+      <span class="material-icons md-24"> chevron_left </span>
+      <span class="material-icons md-24"> chevron_right </span>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+import { ref } from "vue";
+export default {
+  setup() {
+    const currentSlide = ref(3);
+
+    return {
+      currentSlide,
+    };
+  },
+};
 </script>
 
-<style>
+<style lang="scss" scoped>
+.navigation{
+    position: absolute;
+}
 </style>
